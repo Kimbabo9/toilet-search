7308ee8a26f4d7f8529541019c70217a3b0163d0
kimbabo9 <wawa9308@naver.com>
2023-03-26 14:39:52 +0900

    사이드바 오류

--- app/src/main/java/com/example/toilet_search_app/MainActivity.java
+++ app/src/main/java/com/example/toilet_search_app/MainActivity.java
@@ -1,15 +1,94 @@
 package com.example.toilet_search_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        settingSideNavBar();
+    }
+
+    public void settingSideNavBar()
+    {
+        // 툴바 생성
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24);
+        }
+
+        // 사이드 네브바 구현
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                toolbar,
+                R.string.open,
+                R.string.closed
+        );
+
+        // 사이드 네브바 클릭 리스너
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        // -> 사이드 네브바 아이템 클릭 이벤트 설정
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                if (id == R.id.navigation_home){
+                    Toast.makeText(getApplicationContext(), "메인화면", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.navigation_dashboard){
+                    Toast.makeText(getApplicationContext(), "위치찾기", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.navigation_info){
+                    Toast.makeText(getApplicationContext(), "즐겨찾기", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.navigation_analysis){
+                    Toast.makeText(getApplicationContext(), "앱 설정", Toast.LENGTH_SHORT).show();
+                }
+
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+
+
+    /***
+     *  -> 뒤로가기시, 사이드 네브바 닫는 기능
+     */
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
